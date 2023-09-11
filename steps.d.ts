26a7934bf6def08318c151e204cf01e80ec5356e
Przem Wojtusik <przemyslaw.wojtusik@maandag.pl>
2023-09-11 15:15:11 +0200

    update steps

--- steps.d.ts
+++ steps.d.ts
@@ -2,16 +2,17 @@
 type steps_file = typeof import('./steps_file');
 type loginPage = typeof import('./pages/login');
 type productsPage = typeof import('./pages/products');
-type cartPage = typeof import('./pages/cart');
+type cartPage = typeof import('./pages/saucedemo/cart');
 type checkoutPage = typeof import('./pages/checkout');
 type itemViewPage = typeof import('./pages/itemView');
-type headerPage = typeof import('./pages/header');
+type headerPage = typeof import('./pages/headerPage');
+type productsPagePage = typeof import('./pages/productsPage');
 
 declare namespace CodeceptJS {
-  interface SupportObject { I: I, current: any, loginPage: loginPage, productsPage: productsPage, cartPage: cartPage, checkoutPage: checkoutPage, itemViewPage: itemViewPage, headerPage: headerPage }
-  interface Methods extends Playwright { }
-  interface I extends ReturnType<steps_file> { }
+  interface SupportObject { I: I, current: any, loginPage: loginPage, productsPage: productsPage, cartPage: cartPage, checkoutPage: checkoutPage, itemViewPage: itemViewPage, headerPage: headerPage, productsPagePage: productsPagePage }
+  interface Methods extends Playwright {}
+  interface I extends ReturnType<steps_file> {}
   namespace Translation {
-    interface Actions { }
+    interface Actions {}
   }
 }
